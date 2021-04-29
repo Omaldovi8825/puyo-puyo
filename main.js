476d62bd820cb3abfee432f5fbcd16ea1d142a21
Omar Maldonado <omar.maldo.vi@gmail.com>
2021-04-29 16:46:10 -0500

    class puyo pair

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const puyoColors = ['red', 'blue', 'yellow', 'green']
 const puyoRadius = 20
 const boardBottom = board.height - puyoRadius
 //modificar velocidad del juego
-const gameSpeed = 10
+const gameSpeed = 30
 let idCounter = 1
 //profundidad de cada columna
 const colsLimits = [
@@ -26,9 +26,8 @@ class Puyo {
         this.color = puyoColors[Math.floor(Math.random() * puyoColors.length)]
         this.id = idCounter++
         this.rotateCounter = 1
-        this.bottom = colsLimits[((x+20) / this.width) - 1]
-        this.reachedBottom = true
         this.initialPosition = x
+        this.bottom = colsLimits[((this.x + 20) / this.width) - 1]
     }
 
     update(){
@@ -62,36 +61,26 @@ class Puyo {
     }
 
     advance(){
-        if(this.y >= this.bottom){
-            this.update()
-            if(this.reachedBottom){
-                let puyoPosition = (this.x + 20) / this.width
-                colsLimits[puyoPosition - 1] -= this.width
-                puyos.push(new Puyo(this.initialPosition))
-                this.reachedBottom = false
-            }
-        } else {
-            this.y += 1
-            this.update()
+        this.bottom = colsLimits[((this.x + 20) / this.width) - 1]
+        if(this.y === this.bottom){
+            colsLimits[((this.x + 20) / this.width) - 1] -= this.width
+            puyos.push(new Puyo(this.initialPosition))
+            this.y++
         }
+        
+        if(this.y < this.bottom){
+            this.y++
+        }
+
+        this.update()
     }
 
     moveLeft(){
-        const leftLimit = puyoRadius
-        if(this.x <= leftLimit){
-            return false
-        } 
         this.x -= this.width
-        this.bottom = colsLimits[((this.x+20) / this.width) - 1]
     }
 
     moveRight(){
-        const rigthLimit = board.width - puyoRadius
-        if(this.x >= rigthLimit){
-            return false
-        }
         this.x += this.width
-        this.bottom = colsLimits[((this.x+20) / this.width) - 1]
     }
 
     moveDown(){
@@ -106,16 +95,25 @@ document.onkeyup = (e) => {
     const index2 = puyos.length - 1
     switch(e.keyCode){
         case 37:
-            puyos[index1].moveLeft()
-            puyos[index2].moveLeft()
+            if(puyos[index1].x <= puyoRadius  || puyos[index2].x <= puyoRadius){
+                return false
+            } else {
+                puyos[index1].moveLeft()
+                puyos[index2].moveLeft()
+            }
             break
         case 38:
             puyos[index1].rotate()
             // puyos[index2].rotate()
             break
         case 39:
-            puyos[index1].moveRight()
-            puyos[index2].moveRight()
+            const rigthLimit = board.width - puyoRadius
+            if(puyos[index1].x >= rigthLimit || puyos[index2].x >= rigthLimit){
+                return false
+            } else {
+                puyos[index1].moveRight()
+                puyos[index2].moveRight()
+            }
             break
         case 40:
             puyos[index1].moveDown()
